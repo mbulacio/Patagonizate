d10bc6cc4ec9b92484d1adc0486a236d68b808a8
mbulacio <micaelabulacio98@gmail.com>
2021-08-23 01:54:14 -0300

    C:/Program Files/Git/component NavBar Img

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,15 @@
+import './NavBar.css';
+
+function NavBar() {
+    return (
+        <div className="navBar">
+            <p>Provincias</p>
+            <p>Quienes Somos</p>
+            <p>Contacto</p>
+            <p>Carrito</p>
+        </div>
+    )
+}
+
+export default NavBar
+
